7c5d69453fa1d838e6026fc0bf1b87d190412975
Isabelly Lima <mslima5643@gmail.com>
2024-03-10 21:20:23 -0300

    comentarios

--- 2Unidade/TAD/ingressoTAD/tiposbasdados/ingressoTAD/ingresso.h
+++ 2Unidade/TAD/ingressoTAD/tiposbasdados/ingressoTAD/ingresso.h
@@ -1,11 +1,11 @@
 #ifndef INGRESSO_H
 #define INGRESSO_H
 
-typedef struct ingresso Ingresso;
+typedef struct ingresso Ingresso; //redefinição da struc ingresso;
 
-Ingresso* coletardados(int n);
-void altera_preco(Ingresso *i, int n);
-void imprimir(Ingresso *i);
-void imprime_menor_maior_preco(int n, Ingresso *vet);
+Ingresso* coletardados(int n); //função que coleta dados dos ingressos
+void altera_preco(Ingresso *i, int n);//função para alterar preço
+void imprimir(Ingresso *i);//função para imprimir dados dos ingressos
+void imprime_menor_maior_preco(int n, Ingresso *vet);//função para imprimir o preço do maior e menor ingresso
 
 #endif /* INGRESSO_H */
