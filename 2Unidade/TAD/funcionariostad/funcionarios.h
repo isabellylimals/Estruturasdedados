580a37bab8246c989c1a8e57226de7f6bbfaa146
Isabelly Lima <mslima5643@gmail.com>
2024-03-10 21:21:09 -0300

    comentarios

--- 2Unidade/TAD/funcionariostad/funcionarios.h
+++ 2Unidade/TAD/funcionariostad/funcionarios.h
@@ -1,14 +1,14 @@
 #ifndef FUNCIONARIOS_H
 #define FUNCIONARIOS_H
 
-// Definição do tipo Funcionario
+//Definição do tipo Funcionario
 typedef struct {
     char nome[100];
     char departamento[2];
     float salario;
 } Funcionario;
 
-// Função para copiar dados dos funcionários a partir do arquivo de entrada
+//Função para copiar dados dos funcionários
 void copia_dados(FILE* arquivo, int num_funcionarios, Funcionario* pessoal);
 
 #endif // FUNCIONARIOS_H
