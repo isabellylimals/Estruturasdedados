42cea5086b95d7045ce7273a70dd647626937d31
Isabelly Lima <mslima5643@gmail.com>
2024-07-15 13:29:13 -0300

    up

--- 1unidade/aloacao dinamica/praticaalocacao.c
+++ 1unidade/aloacao dinamica/praticaalocacao.c
@@ -6,22 +6,22 @@
 int main() {
     int linhas = 5, colunas = 2, i, j;
 
-    // Alocação das linhas da matriz.
+    
     int **matriz = (int **) malloc(linhas * sizeof(int *));
     if (matriz == NULL) {
-        // Tratar falha na alocação de memória
+    
         return 1;
     }
 
-    // Alocação das colunas e preenchimento da matriz.
+    
     for (i = 0; i < linhas; i++) {
         matriz[i] = (int *) malloc(colunas * sizeof(int));
         for (j = 0; j < colunas; j++) {
-            matriz[i][j] = i * colunas + j; // Cálculo da posição.
+            matriz[i][j] = i * colunas + j; 
         }
     }
 
-    // Exibição da matriz.
+=
   for (i = 0; i < linhas; i++) {
         printf("Matriz [%d]: ", i);
         for (j = 0; j < colunas; j++) {
@@ -29,4 +29,4 @@ int main() {
         }
         printf("\n");
     }
-    
+}
